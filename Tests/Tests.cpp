a31f6d3a276498da0d2a67af9d8384045f4bb662
Svetovoy Andrey <kamaz.jpg@gmail.com>
2019-10-25 19:47:23 +0300

    first commit

---
+++ Tests/Tests.cpp
@@ -0,0 +1,43 @@
+﻿//print как в питоне
+#include <iostream>
+
+template <typename Arg>
+void print(Arg arg)
+{
+	std::cout << arg << '\n';
+}
+
+template <typename Arg, typename... Args>
+void print(Arg arg, Args... args)
+{
+	std::cout << arg << ' ';
+	print(args...);
+}
+
+class A
+{
+protected:
+
+	int t = 10;
+
+public:
+
+
+	void f()
+	{
+		struct B
+		{
+			void g()
+			{
+				print(t); //using t
+			}
+		};
+		B b;
+		b.g();
+	}
+};
+
+int main()
+{
+	print(1, 5, 6);
+}
